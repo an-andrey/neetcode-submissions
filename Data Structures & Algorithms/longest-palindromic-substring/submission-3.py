e4449cbb3c52c9c9fc1481788898955adab8be9c
Andrey Ambartsumov <141183736+an-andrey@users.noreply.github.com>
2026-09-24 15:50:13 -0400

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.py
@@ -0,0 +1,27 @@
+"""
+for every str, check every char as a possible center for palindrome
+but there's even palindromes, so put centers are b/w chars too
+[a,_,b,_,b,_,c] as possible centers for palindrome
+"""
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        max_str = ""
+
+        for i in range(2*len(s)-1):
+            
+            j = int(i/2) if i % 2 == 0 else int((i - 1)/2)
+            k = int(i/2) if i % 2 == 0 else int((i + 1)/2)
+
+            if len(max_str) < 1: 
+                max_str = s[i]
+
+            while j >= 0 and k < len(s) and s[j] == s[k]: 
+                if len(max_str) < len(s[j:k+1]): 
+                    max_str = s[j:k+1]
+
+                j-=1 
+                k+=1
+            
+        return max_str
+            
